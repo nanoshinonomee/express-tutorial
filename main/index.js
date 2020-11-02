1386b18fd4ad505811ae94bccb3c4a4b4c23d4f0
Nano Shinonomee <nanoshinonomee@protonmail.com>
2020-11-02 11:42:03 -0500

    added in the serve-favicon express npm package. Made it work with an example

--- main/index.js
+++ main/index.js
@@ -35,10 +35,9 @@ const expressSession = require('express-session')({
 });
 
 
-
-
-
-
+// we will serve up our favicon
+var favicon = require('serve-favicon');
+app.use(favicon("./favicon/favicon.jpg"));
 
 
 const helmet = require("helmet");
@@ -202,147 +201,13 @@ app.listen(port, () => {        // this will listen on the provided port
     // before I do passport, I need to get a database implemented, looks like I'm going to be using mongoDB
     // I will also use mongoose for easier connecting and data object management
     const passportExampleReq = require('./passportExample');
-    //await passportExampleReq.connectPassportWithMongoose(app);
+    await passportExampleReq.connectPassportWithMongoose(app);
 
     // example using passport called here
     // no longer used
     //passportExampleReq.passportExampleFunc(app);
     // need to do a post
     //passportExampleReq.callPassportExample();
-
-    console.log("pass port here example");
-    // now we can add in our Passport usage after the express sesssion has been created back in index.js
-    app.use(passport.initialize());
-    app.use(passport.session());
-
-    
-    
-
-    // connect up to the database we will be looking to
-    mongoose.connect('mongodb://localhost/MyDatabaseExample', { useNewUrlParser: true, useUnifiedTopology: true });
-
-    
-    // create a schema for holding our data
-    // basically this just defines our datastructure for UserDetail
-    const Schema = mongoose.Schema;
-    const UserDetail = new Schema({
-        username: String,
-        password: String
-    });
-
-    UserDetail.plugin(passportLocalMongoose);   // this inputs the plugin
-
-    //                       1. name param in database of collection   2.ref to schema       3. name assigned to collection in mongoose
-    const UserDetails = mongoose.model('userInfoCollectionNameInDB', UserDetail, 'userInfoCollectionNameInMongoose');     // we will pass in the user information to the USerDetails
-    // the .model creates a model in mongoose for us. 
-    // first parameter is the Name of the collection in the Database
-    
-
-
-
-    // local authentication with passport
-    passport.use(UserDetails.createStrategy()); // creates the local strategy User Details, by passport-local-mongoose
-
-    passport.serializeUser(UserDetails.serializeUser());    // serializes our Users - callback, invoked on auth, stores session via cookie
-
-    passport.deserializeUser(UserDetails.deserializeUser());    // Deserialize User - callback, invoked on request to deserialize and provide unique cookie (credential)
-
-
-    app.post('/loginPassport', (req, res, next) => {
-        passport.authenticate('local',      // this will use the authenticate method which auths with strats using the 'local' parameter
-            (err, user, info) => {
-                if (err) {
-                    console.log("failed1");
-                    return next(err);
-                }
-
-                if (!user) {
-                    console.log("failed2");
-                    return res.redirect('/loginPassport?info=' + info);     // if the auth fails it goes here
-                }
-
-                req.logIn(user, function (err) {
-                    if (err) {
-                        console.log("failed3");
-                        return next(err);
-                    }
-                    console.log("success");
-                    return res.redirect('/homeLogin');  // if success goes here
-                });
-
-            })(req, res, next);
-    });
-
-    app.get('/loginPassport',       // sends the login page with anoyne who gets it
-        (req, res) => res.sendFile('login.html', { root: websiteStaticDirName })
-    );
-
-    app.get('/homeLogin',
-        connectEnsureLogin.ensureLoggedIn('loginPassport'),        // this guards our routes to make sure we are logged in first
-        // connectEnsureLogin.ensureLoggedIn(), NOTE: You can also do it this way just to return to a default home, but we want to redirect to a specific page
-        // using ensureLoggedIn();
-        function(req, res) {
-            console.log("lol");
-            res.sendFile('homeLogin.html', { root: websiteStaticDirName });
-        }
-        //(req, res) => res.sendFile('homeLogin.html', { root: websiteStaticDirName }), 
-    );
-
-    app.get('/private',
-        connectEnsureLogin.ensureLoggedIn('loginPassport'),     // this guards our routes to make sure we are logged in first
-        (req, res) => res.sendFile('private.html', { root: websiteStaticDirName })
-    );
-
-    app.get('/userPassport',
-        connectEnsureLogin.ensureLoggedIn('loginPassport'),     // this guards our routes to make sure we are logged in first
-        (req, res) => res.send({ user: req.user })      // returns information about the user that is currently logged in
-    );
-
-
-    
-
-
-    var userDetailRecords = await UserDetails.find({}); // get all the records in the Model that has been created and connected up to
-    // console.log(userDetailRecords.length);
-    console.log(userDetailRecords[0]);
-
-    // this will do a check first to see if 
-    var userDetailRecords = await UserDetails.find({ username: 'paul'}); // get all the records in the Model that has been created and connected up to
-    //console.log(userDetailRecords.length);
-    if(userDetailRecords.length == 0){
-        //  passport-local-mongoose .register users in our database
-        // using  passport-local-mongoose .register it will salt the password for us
-        UserDetails.register({ username: 'paul', active: false }, 'paul');  // creates the username
-        console.log("user paul has been created");
-
-        // UserDetails.register({ username: 'jay', active: false }, 'jayPassword');
-        // UserDetails.register({ username: 'roy', active: false }, 'royPassword');
-    }
-
-    // lets learn to delete the database, collection, and record
-    var deleteRecord = false;
-    if(deleteRecord){
-        await UserDetails.deleteOne({ username: 'paul'}, function (err) {
-            if (err) return handleError(err);
-            console.log("deleted username paul");
-          });
-    }
-
-    var deleteCollection = false;
-    if(deleteCollection){
-        UserDetails.collection.drop();
-        console.log(`UserDetails collection dropped.`);
-    }
-
-
-    var deleteDb = false;
-    if(deleteDb){
-        await mongoose.connection.db.dropDatabase();
-        console.log(`${mongoose.connection.db.databaseName} database dropped.`);
-    }
-
-
-
 
 
 
